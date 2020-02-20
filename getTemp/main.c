42395b2f592733236f7c04ef88fe770bf0d41b4a
Axek <axel.phanor64@gmail.com>
2020-02-20 11:25:02 +0100

    Ajout de la version 4 getTemp : programme qui récupère les températures des villes

--- getTemp/main.c
+++ getTemp/main.c
@@ -4,24 +4,45 @@
 
 int main(){
 	FILE *fp;
-	char **villes;
 	char *command;
 	int nbVilles = 0;
 	int i = 0;
 	char c;
+	char **villes;
+
+	//---- Importation du fichier texte listesVilles.txt ----//
 	fp = fopen("listeVilles.txt", "r");
 	while((c=fgetc(fp)) != EOF)
 	{	
 		if(c=='\n')
-			nbVilles++;
+			nbVilles++;			
 	}
-	/*
-	while(fscanf(fp, "%s\n", villes[nb]) != -1)
-	{
-
-	}
-	*/	
+	fclose(fp);
+	villes = malloc (nbVilles * sizeof (*villes));
+	for( i = 0; i< nbVilles; i++ ){
+  		villes[i] = (char*)malloc( 50 * sizeof(*villes[i]) );
+ 	}
+	fp = fopen("listeVilles.txt", "r");
+	i = 0;
+	for(i=0;i < nbVilles; i++)
+		fscanf(fp, "%s\n", villes[i]);
 	fclose(fp);
 	printf("%d\n", nbVilles);
+
+	//---- Récupération des données de l'API ----//
+	i = 0;
+	command = malloc(50 * sizeof(char));
+	strcpy(command, "./getTemp ");
+	strcat(command, villes[i]);
+	strcat(command, " > file.txt");
+	printf("%s\n", command);
+	//system(*command);
+
+	//---- Liberation de la mémoire ----//
+	for( i = 0; i < nbVilles; i++ ){
+  		free( villes[i] );
+  		villes[i] = NULL;
+ 	}
+ 	free(villes);
 	return 0;
 }
