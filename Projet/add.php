d021c13395f199361254971ea31407166e58ec51
Axel Phanor <axel.phanor64@gmail.com>
2020-05-04 11:19:48 +0200

    Ajout menu deroulant dynamique

--- Projet/add.php
+++ Projet/add.php
@@ -14,7 +14,7 @@
 	<label for="name">Nom de l'espèce : </label><input id="name" name="name" type="text"/><br><br>
     <label for="temp_min">Température minimal : </label><input id="temp_min" name="temp_minimal" type="text"/><br><br>
     <label for="temp_max">Température maximal : </label><input id="temp_max" name="temp_maximal" type="text"/><br><br>
-	<input type="submit" value="Envoyer"/>  <button type="button" onclick=window.location.href="index.html">Retour</button><br><br>
+	<input type="submit" value="Envoyer"/>  <button type="button" onclick=window.location.href="index.php">Retour</button><br><br>
 	</form>
 	</select>
 </body>
@@ -23,8 +23,7 @@
 <?php
     $globalwarming = new PDO('mysql:host=localhost;dbname=globalwarming;charset=utf8', 'root', '');
     $req = $globalwarming->prepare('INSERT INTO animal(name, temp_min, temp_max) VALUES(?, ?, ?)');
-    $req->execute(array($_POST['name'], $_POST['temp_minimal'], $_POST['temp_maximal']));
-
-
-
+    if(!isset($req)){
+        $req->execute(array($_POST['name'], $_POST['temp_minimal'], $_POST['temp_maximal']));
+    }
 ?>
